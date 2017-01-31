f769af9dc5c4cb5bfc44837b681ebde1f08dc67a
Ian Dees <ian.dees@gmail.com>
2017-01-31 10:26:34 -0500

    Add the ability to include Facebook events

--- make_geojson.py
+++ make_geojson.py
@@ -2,13 +2,15 @@ import arrow
 import boto3
 import json
 import os
+import re
 import requests
-from ics import Calendar
+from ics import Calendar, Event
 
-def get_calendar(url):
-    resp = requests.get(url)
-    resp.raise_for_status()
-    return Calendar(resp.text)
+def json_handler(obj):
+    if isinstance(obj, arrow.Arrow):
+        return obj.isoformat()
+    else:
+        return json.JSONEncoder().default(obj)
 
 def request_geocode(addr_string):
     api_key = os.environ.get('MAPZEN_API_KEY')
@@ -27,7 +29,7 @@ def get_first_geocode_entry(addr_string):
     features = results.get('features')
     return features[0] if features else None
 
-def make_geojson_feature(event):
+def convert_ical_event_to_geojson(event):
     best_geocode = get_first_geocode_entry(event.location)
     if best_geocode:
         geometry = best_geocode['geometry']
@@ -35,8 +37,8 @@ def make_geojson_feature(event):
         geometry = None
 
     properties = {
-        'begin': event.begin.isoformat(),
-        'end': event.end.isoformat(),
+        'begin': event.begin,
+        'end': event.end,
         'name': event.name,
         'description': event.description,
     }
@@ -49,29 +51,104 @@ def make_geojson_feature(event):
 
     return feature
 
+def get_google_ical_events(url):
+    resp = requests.get(url)
+    resp.raise_for_status()
+    events = Calendar(resp.text).events
+
+    return [convert_ical_event_to_geojson(e) for e in events]
+
+def get_facebook_events(url):
+    # The URL we get will probably be a Facebook web URL, so extract the event ID from it
+    match = re.match(r'.*facebook.com/events/(\d+)/?', url)
+    event_id = match.group(1)
+
+    # You can use `app_id|app_secret` as the access_token
+    # to avoid programmatically requesting one:
+    # https://developers.facebook.com/docs/facebook-login/access-tokens/#apptokens
+    merged_fb_tokens = '|'.join([
+        os.environ.get('FACEBOOK_APP_ID'),
+        os.environ.get('FACEBOOK_APP_SECRET')
+    ])
+    resp = requests.get(
+        'https://graph.facebook.com/v2.8/{}'.format(event_id),
+        params=dict(
+            format='json',
+            access_token=merged_fb_tokens,
+        )
+    )
+    resp.raise_for_status()
+
+    event = resp.json()
+
+    properties = {
+        'begin': arrow.get(event.get('start_time')),
+        'end': arrow.get(event.get('end_time')),
+        'name': event.get('name'),
+        'description': event.get('description'),
+    }
+
+    geometry = None
+    if event.get('place'):
+        geometry = {
+            'type': "Point",
+            'coordinates': [
+                event.get('place').get('location').get('longitude'),
+                event.get('place').get('location').get('latitude'),
+            ]
+        }
+
+    feature = {
+        'type': "Feature",
+        'properties': properties,
+        'geometry': geometry
+    }
+
+    # Caller expects a list of features
+    return [feature]
+
+url_action_mapping = [
+    (re.compile(r'^https://calendar.google.com/calendar/ical/.*'), get_google_ical_events),
+    (re.compile(r'^https://www.facebook.com/events/.*'), get_facebook_events),
+]
+
+def get_merged_events():
+    resp = requests.get(os.environ.get('CALENDARS_LIST_URL'))
+    resp.raise_for_status()
+
+    events = []
+    for url in resp.text.splitlines():
+        for regexp, fn in url_action_mapping:
+            if regexp.match(url):
+                events.extend(fn(url))
+
+    return events
+
 def main():
     now = arrow.utcnow()
     geo_features = []
 
-    c = get_calendar('https://calendar.google.com/calendar/ical/resistanceupdates%40gmail.com/public/basic.ics')
+    events = get_merged_events()
+
     # Filter out events that have already ended, sort by event begin time
     events = sorted(
-        (e for e in c.events if e.end >= now),
-        key=lambda e: e.begin
+        (e for e in events if e['properties']['end'] >= now),
+        key=lambda e: e['properties']['begin']
     )
-
-    for e in events:
-        geo_features.append(make_geojson_feature(e))
 
     feature_collection = {
         'type': "FeatureCollection",
-        'features': geo_features,
+        'features': events,
     }
 
     which_bucket = os.environ.get('AWS_S3_BUCKET')
     s3 = boto3.resource('s3')
     s3.Object(which_bucket, 'events.geojson').put(
-        Body=json.dumps(feature_collection, separators=(',', ':')),
+        Body=json.dumps(
+            feature_collection,
+            separators=(',', ':'),
+            default=json_handler,
+        ),
         ACL='public-read',
         ContentType='application/json',
     )
